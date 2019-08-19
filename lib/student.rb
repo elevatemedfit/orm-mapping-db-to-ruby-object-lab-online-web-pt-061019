83873d379a02f2c95f27016bb29ae055bc1d2e29
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 14:44:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade <= 11
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
   def self.find_by_name(name)
         sql = <<-SQL
         SELECT * FROM students
