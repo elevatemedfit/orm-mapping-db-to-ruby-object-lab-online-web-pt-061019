bf2a415fcc0db8a5f2d796eb57ef8d10c74846e5
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 14:56:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,15 @@ class Student
   end
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE COUNT = x
+    SQL
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
+
 
   def self.find_by_name(name)
         sql = <<-SQL
