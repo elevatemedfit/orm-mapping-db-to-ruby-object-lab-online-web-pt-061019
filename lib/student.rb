e44f27594588726972cebdf3d41c07a3f9e7168a
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 14:46:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,9 @@ class Student
     SELECT * FROM students
     WHERE grade <= 11
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
   end
 
 
